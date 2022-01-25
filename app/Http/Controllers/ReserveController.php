45c2ed855f66bdbfa181c3f6c6cc0d182cc7e296
tut171836 <tsuji.satoshi.se@tut.jp>
2022-01-25 12:59:53 +0900

    [fix]store関数の修正

--- app/Http/Controllers/ReserveController.php
+++ app/Http/Controllers/ReserveController.php
@@ -26,15 +26,8 @@ class ReserveController extends Controller
     public function store(Request $request)
     {
         $reserve = new Reserve();
-        $reserve->guest_name        = $request->guest_name;
-        $reserve->start_date_time   = $request->start_date_time;
-        $reserve->end_date_time     = $request->end_date_time;
-        $reserve->purpose           = $request->purpose;
-        $reserve->mail_addr         = $request->mail_addr;
-        $reserve->phone_number      = $request->phone_number;
-        $reserve->guest_num         = $request->guest_num;
-        $reserve->room_id           = $request->room_id;
-        $reserve->save();    
+        Reserve::create($request->pnly(['guest_num', 'start_date_time', 'end_data_time', 'purpose', 'mail_addr', 'phone_number', 'guest_num', 'room_id']));
+        $reserve->save();
         //
     }
 
