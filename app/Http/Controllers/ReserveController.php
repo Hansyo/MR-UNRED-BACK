ee7f20ca4758f4a099ad9aa0c635848247d4c034
tut171836 <tsuji.satoshi.se@tut.jp>
2022-01-25 12:30:37 +0900

    [add]リクエストをデータベースに保存するstore関数を作成した

--- app/Http/Controllers/ReserveController.php
+++ app/Http/Controllers/ReserveController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Reserve;
 
 class ReserveController extends Controller
 {
@@ -24,6 +25,16 @@ class ReserveController extends Controller
      */
     public function store(Request $request)
     {
+        $reserve = new Reserve();
+        $reserve->guest_name        = $request->guest_name;
+        $reserve->start_date_time   = $request->start_date_time;
+        $reserve->end_date_time     = $request->end_date_time;
+        $reserve->purpose           = $request->purpose;
+        $reserve->mail_addr         = $request->mail_addr;
+        $reserve->phone_number      = $request->phone_number;
+        $reserve->guest_num         = $request->guest_num;
+        $reserve->room_id           = $request->room_id;
+        $reserve->save();    
         //
     }
 
