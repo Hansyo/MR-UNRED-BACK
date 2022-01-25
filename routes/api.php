e0b4dc86f1b35f0b3c6ecc633d3615e01d1e7dbe
shimabukuro-takumi <shimabukuro.takumi.uv@tut.jp>
2022-01-25 12:36:42 +0900

    ユーザーのルーティング

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\ReserveController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('user', UserController::class);
+Route::resource('Reservee', ReserveController::class);
