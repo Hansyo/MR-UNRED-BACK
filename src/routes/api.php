742b1de14c2e246dc12ce3f9e6291963d8a55db7
Hansyo <hansyo.souti.1999@gmail.com>
2022-02-14 17:11:29 +0900

    [Fix] CROS

--- src/routes/api.php
+++ src/routes/api.php
@@ -15,7 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['cors'])->group(function () {
-    Route::apiResource('reserve', ReserveController::class);
-    Route::apiResource('repitations', RepitationController::class);
-});
+Route::apiResource('reserve', ReserveController::class);
+Route::apiResource('repitations', RepitationController::class);
