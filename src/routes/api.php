cf74f9fa8d18b2964e4f7b3f030027ea2fe4be2b
Hansyo <hansyo.souti.1999@gmail.com>
2022-02-14 14:44:14 +0900

    [Fix] localhost:3000からのリクエストを受け付けるように対応した

--- src/routes/api.php
+++ src/routes/api.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['cors'])->group(function () {
+    Route::apiResource('reserve', ReserveController::class);
+    Route::apiResource('repitations', RepitationController::class);
 });
-
-Route::apiResource('reserve', ReserveController::class);
-Route::apiResource('repitations', RepitationController::class);
