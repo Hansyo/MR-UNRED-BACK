242c41645107154f3d1a330d5e98d1f47ca687ba
Hansyo <hansyo.souti.1999@gmail.com>
2022-02-18 16:38:39 +0900

    [Update] 会議室名がユニークであることをバリデートした
    データベースレベルでのユニーク制約はやっていません

--- src/app/Http/Requests/StoreRoomRequest.php
+++ src/app/Http/Requests/StoreRoomRequest.php
@@ -24,7 +24,7 @@ class StoreRoomRequest extends FormRequest
     public function rules()
     {
         return [
-            'name'   => ['required', 'string'],
+            'name'   => ['required', 'string', 'unique:rooms'],
             'detail' => ['required', 'string'],
         ];
     }
